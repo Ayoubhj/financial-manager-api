a95015b5c2cbfddabcec6aa92e157e09cc6b762e
Ayoubhj <ayoub21hajouj@gmail.com>
2024-04-15 21:46:22 +0100

    first commit

---
+++ src/main/java/com/financial/financialmanagerapi/FinancialManagerApiApplication.java
@@ -0,0 +1,13 @@
+package com.financial.financialmanagerapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinancialManagerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinancialManagerApiApplication.class, args);
+	}
+
+}
